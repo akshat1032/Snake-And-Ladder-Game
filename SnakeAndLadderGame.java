1b6f096c1159845480adaeb5ca8cdfb99662a694
akshat1032 <akshat.30oct@gmail.com>
2020-09-24 19:50:46 +0530

    Added : Single Player Die Roll Added

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -6,7 +6,14 @@ public class SnakeAndLadderGame {
 		
 		//Variable
 		int position = 0;
+		int dieRoll = 0;
 		
 		System.out.println("Single player game begins with start position : "+position);
+		
+		//Die Rolling
+		while (dieRoll == 0) {
+			
+			dieRoll = (int)(Math.floor(Math.random() * 10) % 7);
+		}
 	}
 }
